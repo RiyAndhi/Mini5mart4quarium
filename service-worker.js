b22985114e3449dddd447cf2b585bcc549e401de
Riyan Andhika <128784894+RiyAndhi@users.noreply.github.com>
2026-05-26 23:27:15 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'smart-aquarium-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/assets/css/style-login.css',
+  '/assets/css/style.css',
+  '/assets/img/icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
